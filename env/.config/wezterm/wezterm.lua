2cc68f34ed9f6f3f3372fb70139c359c8ca3a8f4
Nikosar <randomregistr@gmail.com>
2024-12-25 11:31:24 +0100

    Add wezterm configration files

---
+++ env/.config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+config.default_prog = {'pwsh'}
+-- config.color_scheme = 'Default (dark) (terminal.sexy)'
+config.font = wezterm.font {
+    family = 'JetBrains Mono',
+    weight = 'Medium',
+}
+config.font_size = 14
+config.colors = require("cyberdream")
+
+config.use_fancy_tab_bar = false
+-- config.window_background_opacity = 0.5
+-- config.win32_system_backdrop = 'Acrylic'
+config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+
+return config
