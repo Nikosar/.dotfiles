16b4196e798a2244d9c625e9b86eb6675b7fbb2b
nikosar <randomregistr@gmail.com>
2024-12-27 20:19:12 +0100

    Clean main file

--- env/.config/wezterm/wezterm.lua
+++ env/.config/wezterm/wezterm.lua
@@ -20,42 +20,6 @@ config.tab_bar_at_bottom = true
 config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
 config.audible_bell = 'Disabled'
 
--- Configuring key bindings
-config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
-config.keys = {
-    { key = 'h', mods = "LEADER", action = act.ActivatePaneDirection 'Left' },
-    { key = 'l', mods = "LEADER", action = act.ActivatePaneDirection 'Right' },
-    { key = 'k', mods = "LEADER", action = act.ActivatePaneDirection 'Up' },
-    { key = 'j', mods = "LEADER", action = act.ActivatePaneDirection 'Down' },
-
-    { key = 'v', mods = "LEADER", action = act.ActivateCopyMode },
-    { key = 'c', mods = "LEADER", action = act.SpawnTab 'CurrentPaneDomain' },
-}
-
-for i = 1, 8 do
-  -- LEADER + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'LEADER',
-    action = act.ActivateTab(i - 1),
-  })
-
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = 'CTRL|ALT',
-    action = act.ActivateTab(i - 1),
-  })
-end
-
-local copy_mode = nil
-if wezterm.gui then
-    copy_mode = wezterm.gui.default_key_tables().copy_mode
-    table.insert(
-        copy_mode,
-        { key = 'y', mods = "LEADER", action = wezterm.action.CopyTo 'ClipboardAndPrimarySelection' }
-    )
-end
-
 require("workspace_switcher").apply(config)
 
 return config
